dc450fb2278e74b9eae30ef2597477ce06a7ed48
Andrew Jennison <andrewjohnjennison@gmail.com>
2022-10-03 17:51:17 -0700

    implement solution using pseudoelement instead of injecting HTML

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const testDiv = document.createElement("div");
-testDiv.classList.add("test");
-document.body.appendChild(testDiv);
+// Giving the body element an id allows the stylesheet to use a selector with higher specificity
+// The body selector does not apply the rules
+document.body.setAttribute("id", "body");
